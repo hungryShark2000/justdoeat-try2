5e803535a8d5ec87043181110e86ec4750f56866
NC_Maria NC_Sedunova <NC_maria.sedunova@netcracker.com>
2022-10-09 17:05:03 -0500

    com

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+
+
+function App() {
+    return (
+        <Stack.Navigator screenOptions={{headerShown: false}} >
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Signup" component={Signup} />
+        </Stack.Navigator>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default () => {
+    return (
+        <NavigationContainer>
+
+            <App />
+
+        </NavigationContainer>
+    )
+}
